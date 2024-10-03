5ef2d32603d0eeecd583c7d277678a780bf77446
mcirillo93 <michael.cirillo007@gmail.com>
2024-10-03 14:46:22 -0400

    Create Wheel.js
    Added Wheel.js

---
+++ Wheel.js
@@ -0,0 +1,17 @@
+class Wheel {
+    // Constructor for the Wheel class
+    constructor(diameter = 18, tireBrand = "GoodYear") {
+        this.diameter = diameter;
+        this.tireBrand = tireBrand;
+    }
+    // Getter methods for the properties of the Wheel class
+    get getDiameter() {
+        return this.diameter;
+    }
+    // Setter method for the diameter property
+    get getTireBrand() {
+        return this.tireBrand;
+    }
+}
+// Export the Wheel class
+export default Wheel;
